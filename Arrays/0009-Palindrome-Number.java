8b50bf9a37e31373af88912db4b070511202fbb1
venky4335 <chvenkyvenkatesh3@gmail.com>
2026-06-17 06:58:41 +0530

    Add LeetCode #9 Palindrome Number solution

---
+++ Arrays/0009-Palindrome-Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int res = 0;
+        int temp = x;
+        while(x > 0){
+            int rem = x % 10;
+            res = res*10+rem;
+            x /= 10;
+        }
+        return (res == temp);
+    }
+}
